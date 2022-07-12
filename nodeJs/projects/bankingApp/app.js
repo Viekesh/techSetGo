6f935c71a24b2a3778b98d3e5b25e5e3b3fa5202
viekesh <vikeshgaikwad1@gmail.com>
2022-07-12 18:07:06 +0530

    Continue To Code bankingapp CLI

--- nodeJs/projects/bankingApp/app.js
+++ nodeJs/projects/bankingApp/app.js
@@ -1,8 +1,40 @@
-console.log("Welcome to the banking app");
-console.log("\n 1. Creating new account");
-console.log("\n 2. Deposite money");
-console.log("\n 3. Withdraw money");
-console.log("\n 4. Check balance");
-console.log("\n 5. Transfer money");
-console.log("\n 6. Exit");
-console.log("\n Enter your choice");
+// import readline from "readline";
+
+
+
+// console.log("Welcome to the banking app");
+// console.log("\n 1. Creating new account");
+// console.log("\n 2. Deposite money");
+// console.log("\n 3. Withdraw money");
+// console.log("\n 4. Check balance");
+// console.log("\n 5. Transfer money");
+// console.log("\n 6. Exit");
+
+
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// let choice = 0;
+// while () {
+//     if (choice >= 6) {
+//         process.exit(0);
+//     } else {
+//         rl.question("\n Enter your choice : ", ch => {
+//             console.log(`you entered ${ch}`);
+//             rl.close();
+//         });
+//     }
+// }
+
+
+
+// rl.on("close", () => {
+//     console.log("\n Bye Bye");
+//     process.exit(0);
+// });
+
+
+// // because of while(true) the above code is not recheable 
