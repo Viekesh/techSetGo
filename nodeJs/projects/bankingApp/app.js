386463cf119d96e10cc2650d19453703c4707918
viekesh <vikeshgaikwad1@gmail.com>
2022-07-12 12:54:21 +0530

    bankingApp CLI Initial Commit

---
+++ nodeJs/projects/bankingApp/app.js
@@ -0,0 +1,8 @@
+console.log("Welcome to the banking app");
+console.log("\n 1. Creating new account");
+console.log("\n 2. Deposite money");
+console.log("\n 3. Withdraw money");
+console.log("\n 4. Check balance");
+console.log("\n 5. Transfer money");
+console.log("\n 6. Exit");
+console.log("\n Enter your choice");
